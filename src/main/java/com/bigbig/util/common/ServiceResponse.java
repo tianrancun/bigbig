4783da815cdd1bd47ab817d73e69c94261f4da09
779151588@qq.com <- Vendor vn0ow6b@HOMEOFFICE.wal-mart.com>
2019-03-29 15:21:15 +0800

    up

---
+++ src/main/java/com/bigbig/util/common/ServiceResponse.java
@@ -0,0 +1,28 @@
+package com.bigbig.util.common;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ServiceResponse<T> {
+	
+	private Status status = Status.SUCCESS;
+	
+	private String errorMessge;
+	
+	private T payload;
+	
+	private String printsPayload;
+	
+	public ServiceResponse(T payload){
+		this.payload = payload;
+	}
+	
+	public ServiceResponse(String errorMessage) {
+		this.status = Status.ERROR;
+		this.errorMessge = errorMessage;
+	}
+}
