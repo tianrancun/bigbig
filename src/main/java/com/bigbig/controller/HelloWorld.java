ddfc38b20610aabcaead1cd320784a673dfca9a5
779151588@qq.com <- Vendor vn0ow6b@HOMEOFFICE.wal-mart.com>
2019-03-29 14:26:42 +0800

    init

--- src/main/java/com/bigbig/controller/HelloWorld.java
+++ src/main/java/com/bigbig/controller/HelloWorld.java
@@ -1,15 +1,18 @@
 package com.bigbig.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
 @RequestMapping(value = "say")
+@Slf4j
 public class HelloWorld {
 
     @RequestMapping(value = "/hello")
     public String sayHelloWorld(){
-        return "hello world";
+        log.info("say hello world!");
+        return "hello world!";
     }
 }
