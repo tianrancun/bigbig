643553bd17925218f8d61699069064ba57df89dd
vn0ow6b <- Vendor vn0ow6b@HOMEOFFICE.wal-mart.com>
2019-04-16 16:19:35 +0800

    swagger

--- src/main/java/com/bigbig/service/PersonService.java
+++ src/main/java/com/bigbig/service/PersonService.java
@@ -3,14 +3,19 @@ package com.bigbig.service;
 import com.bigbig.entity.Cards;
 import com.bigbig.entity.Person;
 import com.bigbig.repository.PersonRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 @RestController
+@Slf4j
 public class PersonService {
     @Autowired
     private PersonRepository personRepository;
@@ -23,6 +28,31 @@ public class PersonService {
             System.out.println(card.getId());
             System.out.println(card.getNum());
         }
+        return person;
+    }
+
+    @PostMapping(value = "/add_person")
+    public Person addPerson(@RequestParam("userName") final String userName){
+        Person person= new Person();
+        String uid = System.currentTimeMillis()+"";
+        log.info("uid {}",uid);
+        person.setId(uid);
+        person.setUsername(userName);
+        person.setAge("10");
+
+        List<Cards> cards = new ArrayList<>();
+        Cards c1 = new Cards();
+        c1.setId(uid);
+        c1.setNum("111111111111");
+        cards.add(c1);
+        Cards c2 = new Cards();
+        c2.setId(uid);
+        c2.setNum("111111111112");
+        cards.add(c1);
+        cards.add(c2);
+        person.setCards(cards);
+
+        personRepository.save(person);
         return person;
     }
 }
